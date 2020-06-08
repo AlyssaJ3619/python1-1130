144d1768ecb1ae1bbfa541b7d1057d49a735db39
Victoria-Ou <vicyou12@gmail.com>
2020-06-07 22:40:12 -0500

    20200604homework

---
+++ Victoria/20200604homework.py
@@ -0,0 +1,81 @@
+print("Math League 12023")
+
+a = 2020
+b = 20
+c = a-b
+print ("1. 2020 - 20 =", c)
+
+a = 494
+b = 118
+c = a+b
+print("2. 494 + 118 =",  c)
+
+print("3. The tens digit of 1296 is 9")
+
+a = 60
+b = 5
+c = a/b
+print("4. 60 / 5 =", c)
+
+a = 14
+b = 50
+c = 14*50
+print("5. 14 * 50 =", c)
+
+import math
+a = 66
+b = 4
+c = (math.remainder(a, b))
+print("6. The remainder of 66 / 4 is", c)
+
+a = 14
+b = 3
+c = a*b
+print("7. The product of 14 and 3 is", c)
+
+a = 8
+b = 11
+c = a*b+a
+print("8. 8 * 11 + 8 =", c)
+
+a = 53
+b = 12
+c = 28
+d = 7
+e = a+b+c+d
+print("9. 53 + 12 + 28 + 7 =", e)
+
+a = round(201/100)*100
+b = round(402/100)*100
+c = round(804/100)*100
+d = round(603/100)*100
+e = a+b+c+d
+print("10. (estimate) 201 + 402 + 804 + 603 =", e)
+
+a = round(1987/10)*10
+print("11. 1987 rounded to the nearest ten is", a)
+
+a = 17
+b = a**2
+print("12. 17^2 =", b)
+
+a = 578
+b = 361
+c = 27
+d = a-b-c
+print("13. 578 - 361 - 27 =", d)
+
+a = 11
+b = 48
+c = a*b
+print("14. 11 * 48 =", c)
+
+a = 681
+b = 292
+c = a-b
+print("15. 681 - 292 = ", c)
+
+a = 45
+b = 18
+c = a*b
+print("16. 45 * 18 =", c)
